7c8f1627ddf1e0233b9b9a4c630bc22f0d672653
Viktor Pyshnenko <vpyshnenko@gmail.com>
2020-11-13 11:43:18 +0200

    Hello Alice

--- 2/src/index.js
+++ 2/src/index.js
@@ -1,22 +1,3 @@
-// import { Server } from "./server.js";
-// import { Client } from "./client.js";
-
-// const u1 = { username: "Alice", password: "FG%^&GFRE$" };
-// const u2 = { username: "Bob", password: "l;kj(*UYHYG%^F" };
-
-// const s = (window.s = new Server());
-// s.register(u1);
-// s.register(u2);
-
-// const c1 = (window.c1 = new Client(u1));
-// const c2 = (window.c2 = new Client(u2));
-
-// c1.connect(s);
-// c2.connect(s);
-// c2.onMessage((msg) => console.log("incoming message: ", msg));
-// c1.send({ to: u2.username, body: "Hello world" });
-// c2.send({ to: u1.username, message: "Hi" });
-
 import { UserService } from "./server/UserService.js";
 import { SessionService } from "./server/SessionService.js";
 import { MessageService } from "./server/MessageService.js";
@@ -40,6 +21,7 @@ const c2 = new Client({ services: { sessionService, messageService }, ...u2 });
 c1.connect();
 c1.onMessage((msg) => console.log("c1 incoming message: ", msg));
 c2.onMessage((msg) => console.log("c2 incoming message: ", msg));
-c1.send({ to: u2.username, body: "Hello world" });
+c1.send({ to: u2.username, body: "Hello Alice" });
+c1.send({ to: u2.username, body: "How are you?" });
 c2.connect();
-c2.send({ to: u1.username, body: "Hi" });
+c2.send({ to: u1.username, body: "Hi, I'm ok" });
